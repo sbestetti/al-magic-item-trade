b75b47ccf697a007ae79533c47fdec85b18da1a6
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-05-26 08:35:10 +0100

    Route to create test DB easily

--- src/start.py
+++ src/start.py
@@ -1,17 +1,27 @@
 from flask import Flask, jsonify
 
+from models import db
 
 app = Flask(__name__)
 app.config.from_pyfile("./settings.py")
 
+db.init_app(app)
 
 @app.route("/healthz")
 def healthz():
-    return jsonify(
-        {
-            "Status": "OK"
-        }
-    ), 200
+    return jsonify({
+        "Status": "OK"
+    }), 200
+
+
+# TODO: Remove before prod
+@app.route("/create_db")
+def create_db():
+    try:
+        db.create_all()
+        return "Done", 200
+    except Exception as e:
+        return f"Error creating DB: {e}", 500
 
 
 if __name__ == "__main__":

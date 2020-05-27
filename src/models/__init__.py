b710992f5b9377edba8d9f99982939c39da652de
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-05-27 17:43:21 +0100

    Moved User class to Models package root

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,4 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-from . import user
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    dci = db.Column(db.String(10), nullable=False, unique=True)
+    active = db.Column(db.Boolean, default=True)
+    verified = db.Column(db.Boolean, default=False)
+    last_login = db.Column(db.DateTime)

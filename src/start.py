a7aba797abe3461f1c16f2dbf0c61587eced8556
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-05-29 08:52:31 +0100

    Added test user creation

--- src/start.py
+++ src/start.py
@@ -24,5 +24,31 @@ def create_db():
         return f"Error creating DB: {e}", 500
 
 
+@app.route("/run_tests")
+def run_tests():
+    from datetime import datetime
+    from models import User, Character
+
+    new_user = User(
+        name="Sergio",
+        email="sergio@email.com",
+        dci="12345678",
+        active=True,
+        verified=False,
+        last_login=datetime.now()
+        )
+
+    new_character = Character(
+        name="Tinemir",
+        user=new_user
+        )
+
+    db.session.add(new_user)
+    db.session.add(new_character)
+    db.session.commit()
+
+    return "Finished", 200
+
+
 if __name__ == "__main__":
     app.run(debug=True)

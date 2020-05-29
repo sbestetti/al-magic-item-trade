a0c572fa5e488ca9253db2c9ff93f50491ef7414
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-05-29 15:47:18 +0100

    Inventory table

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,6 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+inventory = db.Table(
+    "inventory",
+    db.Column("inventory_id", db.Integer, primary_key=True),
+    db.Column(
+        "character_id",
+        db.Integer,
+        db.ForeignKey("character.character_id")),
+    db.Column("item_id", db.Integer, db.ForeignKey("item.item_id")),
+)
+
+
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -22,10 +33,10 @@ class Character(db.Model):
     user_id = db.Column(
         db.Integer,
         db.ForeignKey(User.user_id),
-        nullable=False
-        )
+        nullable=False)
 
     user = db.relationship("User", backref=db.backref("user"), lazy=True)
+    items = db.relationship("Item", secondary=inventory, lazy="subquery")
 
 
 class Item(db.Model):
@@ -36,3 +47,8 @@ class Item(db.Model):
     attuned = db.Column(db.Boolean, nullable=False)
     notes = db.Column(db.String(300), nullable=False)
     source = db.Column(db.String(10), nullable=False)
+
+    characters = db.relationship(
+        "Character",
+        secondary=inventory,
+        lazy="subquery")

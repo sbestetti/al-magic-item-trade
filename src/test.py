be01d9297c0b86238d1f96d3111858968193b091
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-08-10 10:01:45 +0100

    Moved database setup to test module

--- src/test.py
+++ src/test.py
@@ -3,14 +3,99 @@ Module containing all the unit tests. Can be run by executing
 "python -m unittest" on src directory
 """
 import unittest
+from datetime import datetime
 
 import app_factory
+from models import db, User, Character, Item, Offer
 
 
 class TestMain(unittest.TestCase):
 
     def setUp(self):
         self.app = app_factory.get_app()
+        with self.app.app_context():
+            db.init_app(self.app)
+            db.drop_all()
+            db.create_all()
+
+            # Creating test users
+            new_user_1 = User(
+                name="Sergio",
+                email="sergio@email.com",
+                dci="12345678",
+                active=True,
+                verified=False,
+                last_login=datetime.now()
+            )
+            new_user_2 = User(
+                name="Rosca",
+                email="rosca@email.com",
+                dci="910111213",
+                active=True,
+                verified=False,
+                last_login=datetime.now()
+                )
+
+            # Creating test characters
+            new_character_1 = Character(
+                name="Tinemir",
+                )
+
+            new_character_2 = Character(
+                name="Uther",
+            )
+
+            new_character_3 = Character(
+                name="Rosquette",
+            )
+
+            # Creating test items
+            new_item_1 = Item(
+                name="Magic Sword",
+                type_="Weapon",
+                rarity="Unique",
+                attuned=True,
+                notes="Bleeds",
+                source="DMG"
+            )
+
+            new_item_2 = Item(
+                name="Magic Staff",
+                type_="Weapon",
+                rarity="Rare",
+                attuned=True,
+                notes="Bleeds",
+                source="DMG"
+            )
+
+            new_item_3 = Item(
+                name="Magic beads",
+                type_="Weapon",
+                rarity="Uncommon",
+                attuned=True,
+                notes="Bleeds",
+                source="DMG"
+            )
+
+            # Commiting all objects to DB
+            new_character_1.items.append(new_item_1)
+            new_character_1.items.append(new_item_2)
+            new_character_3.items.append(new_item_3)
+            new_user_1.characters.append(new_character_1)
+            new_user_1.characters.append(new_character_2)
+            new_user_2.characters.append(new_character_3)
+            db.session.add(new_user_1)
+            db.session.add(new_user_2)
+            db.session.commit()
+
+            # Creating Offers
+            new_offer_1 = Offer(
+                offered_item=new_item_1.item_id,
+                wanted_item=new_item_3.item_id,
+                date_created=datetime.now(),
+            )
+            db.session.add(new_offer_1)
+            db.session.commit()
 
     def test_status(self):
         """

b2d4d780e2a4f3771192b387ec86edcbdf98a6a5
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-08-14 22:02:49 +0100

    Writing user to the DB

--- src/views/register.py
+++ src/views/register.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 
+from sqlalchemy.exc import IntegrityError
+
 from forms import Registration_Form
-from models import User
+from models import User, db
+
 
 bp = Blueprint("register", __name__, url_prefix="/register")
 
@@ -16,6 +19,12 @@ def register():
         user.dci = form.dci.data
         user.active = True
         user.verified = False
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except IntegrityError:
+            flash("User already exists")
+            return render_template("register.html", form=form)
         flash("Please login")
         return redirect(url_for("index.index"))
     return render_template("register.html", form=form)

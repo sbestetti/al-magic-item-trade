237c1b4b3d0e36f53c030a93043f7a68955c6e29
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-08-14 18:12:54 +0100

    User object creation

--- src/views/register.py
+++ src/views/register.py
@@ -1,16 +1,21 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, flash, redirect, url_for
 
 from forms import Registration_Form
+from models import User
 
 bp = Blueprint("register", __name__, url_prefix="/register")
 
 
 @bp.route("/", methods=["GET", "POST"])
 def register():
-    print(request.form)
     form = Registration_Form()
     if form.validate_on_submit():
-        print("Form validated: {request.form[0][0]}")
-    else:
-        print("Not validated")
+        user = User()
+        user.name = form.name.data
+        user.email = form.email.data
+        user.dci = form.dci.data
+        user.active = True
+        user.verified = False
+        flash("Please login")
+        return redirect(url_for("index.index"))
     return render_template("register.html", form=form)

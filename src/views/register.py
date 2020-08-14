c6e2676203ff3495e9a67669b2327b70e187b2e5
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-08-14 10:09:57 +0100

    IF for testing form submission

--- src/views/register.py
+++ src/views/register.py
@@ -5,7 +5,12 @@ from forms import Registration_Form
 bp = Blueprint("register", __name__, url_prefix="/register")
 
 
-@bp.route("/")
+@bp.route("/", methods=["GET", "POST"])
 def register():
-    form = Registration_Form(request.form)
+    print(request.form)
+    form = Registration_Form()
+    if form.validate_on_submit():
+        print("Form validated: {request.form[0][0]}")
+    else:
+        print("Not validated")
     return render_template("register.html", form=form)

cf42f898366f2bd53588cd75c4693e3802fa3d89
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-08-24 11:01:53 +0100

    DAO module

--- src/dao.py
+++ src/dao.py
@@ -1,3 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.exc import IntegrityError
 
 db = SQLAlchemy()
+
+
+def add_user(user):
+    user.password = generate_password_hash(user.password)
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except IntegrityError:
+        raise IntegrityError
+    return None

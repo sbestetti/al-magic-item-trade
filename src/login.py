8ab1fbab3ea163c9383441fdc487ce563a6a7b51
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-08-25 16:27:13 +0100

    Fixed user query

--- src/login.py
+++ src/login.py
@@ -7,5 +7,4 @@ login_manager = LoginManager()
 
 @login_manager.user_loader
 def load_user(user_email):
-    user = User.query.filter_by(email=user_email).first()
-    return user
+    return User.query.filter_by(email=user_email).first()

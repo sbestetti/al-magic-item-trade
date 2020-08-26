573ef5cfccd66c26f1357d3dac8eb5fc2ff7cb94
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-08-26 16:32:17 +0100

    Dashboard page and view

--- src/views/dashboard.py
+++ src/views/dashboard.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 
-from flask_login import login_required
+from flask_login import login_required, current_user
+from models import Character
 
 bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
@@ -8,4 +9,5 @@ bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 @bp.route("/")
 @login_required
 def dashboard():
-    return "Dashboard loaded", 200
+    chars = Character.query.filter_by(user=current_user).all()
+    return render_template("dashboard.html", characters=chars)

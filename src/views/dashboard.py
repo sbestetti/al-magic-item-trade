efcd8daccebf253ca6cad772aa40e37397018be1
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-08-27 16:17:45 +0100

    Added view for character creation

--- src/views/dashboard.py
+++ src/views/dashboard.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
-from models import Character
+
+from models import Character, Level
+from forms import Character_Form
+from dao import db
+
 
 bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
@@ -11,3 +14,24 @@ bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 def dashboard():
     chars = Character.query.filter_by(user=current_user).all()
     return render_template("dashboard.html", characters=chars)
+
+
+@bp.route("/add_char", methods=["POST", "GET"])
+@login_required
+def add_char():
+    form = Character_Form()
+    if form.validate_on_submit():
+        char = Character(
+            name=form.name.data,
+            race=form.race.data,
+        )
+        level = Level(
+            class_=form.main_class.data,
+            levels=form.level.data
+        )
+        char.levels.append(level)
+        char.user_id = current_user.user_id
+        db.session.add(char)
+        db.session.commit()
+        return redirect(url_for(".dashboard"))
+    return render_template("add_char.html", form=form)

3f2ab3350469f8492351fd86b0b4adf51c2478fe
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-09-07 15:54:13 +0100

    Added items to the dashboard

--- src/views/dashboard.py
+++ src/views/dashboard.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 
-from models import User, Item
+from models import User, Item, Item_Model
 
 
 bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
@@ -10,5 +10,10 @@ bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 @bp.route("/")
 @login_required
 def dashboard():
-    user = current_user
-    return render_template("dashboard.html", user=current_user)
+    items = list()
+    for item in current_user.items:
+        current_model = Item_Model.query.filter_by(
+            item_model_id=item.item_model_id
+            ).first()
+        items.append(current_model)
+    return render_template("dashboard.html", user=current_user, items=items)

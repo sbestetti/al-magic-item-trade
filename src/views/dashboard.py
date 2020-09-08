0e02a716696b033bd58ba04e6af04028ecd0f0f5
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-09-08 17:57:04 +0100

    View for new item

--- src/views/dashboard.py
+++ src/views/dashboard.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 
 from models import User, Item, Item_Model
+from forms import New_Item_Form
 
 
 bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
@@ -17,3 +18,14 @@ def dashboard():
             ).first()
         items.append(current_model)
     return render_template("dashboard.html", user=current_user, items=items)
+
+
+@bp.route("/new_item", methods=["POST", "GET"])
+@login_required
+def new_item():
+    item_options = list()
+    for item in Item_Model.query.all():
+        item_options.append(item.name)
+    form = New_Item_Form()
+    form.item.choices = item_options
+    return render_template("new_item.html", form=form)
